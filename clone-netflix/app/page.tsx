32e40d508904f205f26fbff6a32e5113af26d4ce
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-09 10:50:06 -0300

    primer!

--- clone-netflix/app/page.tsx
+++ clone-netflix/app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
   return (
     <div className="relative bg-zinc-900">
       <NavBar />
-      <div className="h-screen"></div>
-      <div className="h-screen"></div>
-      <div className="h-screen"></div>
-      <div className="h-screen"></div>
+
+
+      <div className="h-screen">1a</div>
+      <div className="h-screen">a</div>
+      <div className="h-screen">a</div>
+      <div className="h-screen">a</div>
     </div>
   );
 }
