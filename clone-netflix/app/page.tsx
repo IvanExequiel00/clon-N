e35580a254e339f4cc9a49ac6d4032ae16d6c4f2
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-09 09:24:02 -0300

    home changes

--- clone-netflix/app/page.tsx
+++ clone-netflix/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div className="relative bg-zinc-900">
       <NavBar />
+      <div className="h-screen"></div>
+      <div className="h-screen"></div>
+      <div className="h-screen"></div>
+      <div className="h-screen"></div>
     </div>
   );
 }
