5aea170f36f29ec5437181b2d71ee15164dd04df
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-21 17:16:22 -0300

    a

--- clone-netflix/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
+++ clone-netflix/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
@@ -2,10 +2,19 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormAddProfileProps } from "./FormAddProfile.type";
 import { z } from "zod";
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
 
 export function FormAddProfile(props: FormAddProfileProps) {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -20,5 +29,27 @@ export function FormAddProfile(props: FormAddProfileProps) {
   };
 
   const { setOpen } = props;
-  return <div>FormAddProfile</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 }
