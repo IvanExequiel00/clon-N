f18276b524bf0445f8c2d8b73c24f979b90af0d0
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-21 17:33:05 -0300

    6

--- clone-netflix/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
+++ clone-netflix/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
@@ -2,8 +2,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormAddProfileProps } from "./FormAddProfile.type";
 import { z } from "zod";
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,20 +12,48 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { formSchema } from "./FormAddProfile.form";
+import { useState } from "react";
+import { toast } from "@/hooks/use-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export function FormAddProfile(props: FormAddProfileProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();  
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      profileName: "",
+      avatarUrl: undefined,
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post("/api/userNetflix", values);
+      if (response.status !== 200) {
+        toast({
+          title: "Ops! Ha ocurrido un error",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "¡Usuario creado correctamente!",
+        });
+      }
+      router.refresh();
+      setOpen(false);
+    } catch (error) {
+      console.log(error);
+      toast({ title: "Ops! Ha ocurrido un error", variant: "destructive" });
+
+      setIsLoading(false);
+    }
   };
 
   const { setOpen } = props;
@@ -51,5 +79,5 @@ export function FormAddProfile(props: FormAddProfileProps) {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
