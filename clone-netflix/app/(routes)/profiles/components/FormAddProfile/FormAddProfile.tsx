e89bf76501ca93477f8326cc15fd12d30c9c298e
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-22 14:12:05 -0300

    a

--- clone-netflix/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
+++ clone-netflix/app/(routes)/profiles/components/FormAddProfile/FormAddProfile.tsx
@@ -1,28 +1,34 @@
 "use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FormAddProfileProps } from "./FormAddProfile.type";
-import { z } from "zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
+import { FormAddProfileProps } from "./FormAddProfile.type";
 import { formSchema } from "./FormAddProfile.form";
-import { useState } from "react";
-import { toast } from "@/hooks/use-toast";
-import axios from "axios";
+import { dataProfilesImages } from "./FormAddProfile.data";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "@/hooks/use-toast";
 
 export function FormAddProfile(props: FormAddProfileProps) {
+  const { setOpen } = props;
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();  
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -56,27 +62,66 @@ export function FormAddProfile(props: FormAddProfileProps) {
     }
   };
 
-  const { setOpen } = props;
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="profileName"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Nombre de perfil</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="Juan" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="avatarUrl"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Selecciona tu imagen de perfil</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex space-y-1"
+                >
+                  {dataProfilesImages.map((data) => (
+                    <FormItem
+                      key={data.urlImage}
+                      className="flex flex-col-reverse justify-center items-center space-x-5 space-y-0 cursor-pointer"
+                    >
+                      <FormControl className="text-white ml-3 mt-1">
+                        <RadioGroupItem value={data.urlImage} />
+                      </FormControl>
+                      <FormLabel className="font-normal flex justify-center w-full">
+                        <Image
+                          src={data.urlImage}
+                          alt="Profile"
+                          width={50}
+                          height={50}
+                          className={
+                            field.value === data.urlImage
+                              ? "cursor-pointer border-white border"
+                              : ""
+                          }
+                        />
+                      </FormLabel>
+                    </FormItem>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isLoading}>
+          Crear usuario
+        </Button>
       </form>
     </Form>
   );
