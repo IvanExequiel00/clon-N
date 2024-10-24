f66ba76439c4c6edd6fb6f9efb061f4ff50021ff
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-24 14:50:11 -0300

    a

--- clone-netflix/app/(routes)/upload-movie/page.tsx
+++ clone-netflix/app/(routes)/upload-movie/page.tsx
@@ -1,7 +1,14 @@
+import { Logo } from "@/components/shared/Logo";
 
 
 export default function UploadMoviesPage() {
   return (
-    <div>UploadMoviesPage</div>
+    <div className="bg-zinc-900  h-full flex flex-col justify-center items-center
+    ">
+        <Logo />
+        <h1 className="text-2xl my-8 font-semibold">
+            Sube tus peliculas fav
+        </h1>
+    </div>
   )
 }
