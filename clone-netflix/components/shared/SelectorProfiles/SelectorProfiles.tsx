8504ba94324c6aefb01f3d4a396d6aa1e7b9fbc5
IvanRojas FT47b <exequieliv@outlook.es>
2024-10-29 16:32:36 -0300

    a

--- clone-netflix/components/shared/SelectorProfiles/SelectorProfiles.tsx
+++ clone-netflix/components/shared/SelectorProfiles/SelectorProfiles.tsx
@@ -10,14 +10,16 @@ import {
 import { useCurrentNetflixUser } from "@/hooks/use-current-user";
 import { UserNetflix } from "@prisma/client";
 import Image from "next/image";
-import { ChevronDown, Pencil } from "lucide-react";
+import { ChevronDown, LogOut, Pencil } from "lucide-react";
+import { signOut } from "next-auth/react";
 
 export function SelectorProfiles(props: SelectorProfileProps) {
   const { users } = props;
   const router = useRouter();
   const { changeCurrentUser, currentUser } = useCurrentNetflixUser();
-  const onChange = (userNetflix: UserNetflix) => {
-    console.log("change users");
+  const onChangeUser = (userNetflix: UserNetflix) => {
+    changeCurrentUser(userNetflix);
+    router.refresh();
   };
   return (
     <DropdownMenu>
@@ -31,22 +33,35 @@ export function SelectorProfiles(props: SelectorProfileProps) {
             width={35}
             height={35}
           />
-          <ChevronDown className=""/>
+          <ChevronDown className="" />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 p-2 bg-black/80 border-transparent">
-     {users.map((user) =>{
-      <DropdownMenuItem key={user.id} onclick={() => onChangeUser(user)}
-      className="flex gap-2 mb-3 group"
-      >
-<Image src={user.avatarUrl} alt="Profile Image" width={30} height={30}/>
-<p className="text-white group-hover:text-black">{user.profileName}</p>
-      </DropdownMenuItem>
-     })}
-     <DropdownMenuItem>
-      <Pencil className="w-4 h-4"/>
-      Administrar Perfil
-     </DropdownMenuItem>
+        {users.map((user) => (
+          <DropdownMenuItem
+            key={user.id}
+            onClick={() => onChangeUser(user)}
+            className="flex gap-2 mb-3 group"
+          >
+            <Image
+              src={user.avatarUrl}
+              alt="Profile Image"
+              width={30}
+              height={30}
+            />
+            <p className="text-white group-hover:text-black">
+              {user.profileName}
+            </p>
+          </DropdownMenuItem>
+        ))}
+        <DropdownMenuItem onClick={() => router.push("/profiles")} className="flex gap-2 mb-3 group text-white cursor-pointer">
+          <Pencil className="w-4 h-4" />
+          Administrar Perfil
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()} className="flex gap-2 mb-3 group text-white cursor-pointer">
+          <LogOut className="w-4 h-4"/>
+          Cerrar Sesion
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
