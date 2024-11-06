d9da5bc29c4743c092fffa8614db72e6307ea4ca
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-06 16:53:48 -0300

    a

---
+++ clone-netflix/components/shared/BlockMovies/BlockMovies.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BlockMoviesProps } from './BlockMovies.types'
+
+export  function BlockMovies(props:BlockMoviesProps) {
+    const {title, movies, isMyList} = props;
+  return (
+    <div className='-top-16 relative px-[4%] md:pt-20 md:pb-20 overflow-auto bg-[#171717]'>
+        <h3>{title}</h3>
+    </div>
+  )
+}
