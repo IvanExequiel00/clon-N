f7a621655d9df651665c7b6661862fb13a0b5f8f
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-06 16:54:12 -0300

    a

--- clone-netflix/app/(routes)/(home)/components/ListMovies/ListMovies.tsx
+++ clone-netflix/app/(routes)/(home)/components/ListMovies/ListMovies.tsx
@@ -1,9 +1,12 @@
+import { BlockMovies } from "@/components/shared/BlockMovies/BlockMovies";
 import { ListMoviesProps } from "./ListMovies.types";
 
 
 export  function ListMovies(props: ListMoviesProps) {
     const { movies } = props;
   return (
-    <div>ListMovies</div>
+    <div>
+        <BlockMovies title="Peliculas mas recientes" movies={movies} isMyList={false}/>
+    </div>
   )
 }
