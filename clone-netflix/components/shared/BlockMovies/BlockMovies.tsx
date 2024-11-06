89ae13b624279aae4c06330d9593744a54ba5100
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-06 17:03:10 -0300

    a

--- clone-netflix/components/shared/BlockMovies/BlockMovies.tsx
+++ clone-netflix/components/shared/BlockMovies/BlockMovies.tsx
@@ -5,7 +5,7 @@ export  function BlockMovies(props:BlockMoviesProps) {
     const {title, movies, isMyList} = props;
   return (
     <div className='-top-16 relative px-[4%] md:pt-20 md:pb-20 overflow-auto bg-[#171717]'>
-        <h3>{title}</h3>
+        <h3 className='text-2xl font-semibold mb-3 '>{title}</h3>
     </div>
   )
 }
