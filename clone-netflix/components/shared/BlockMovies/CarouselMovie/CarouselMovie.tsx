fa21dfd9191d5f841afad0497c51706bd68857ec
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-07 12:17:39 -0300

    blockmoviescarrusel

--- clone-netflix/components/shared/BlockMovies/CarouselMovie/CarouselMovie.tsx
+++ clone-netflix/components/shared/BlockMovies/CarouselMovie/CarouselMovie.tsx
@@ -23,9 +23,22 @@ export  function CarouselMovie(props: CarouselMovieProps) {
 <Image 
 fill
 sizes='(max-width: 768px) 100vw, 50vw'
+className='rounded-md'
 src={movie.thumbnailUrl}
 alt='Image'
 />
+<div className='opacity-0 absolute top-0 transition-all 
+duration-300 z-10 invisible sm:visible delay-300 w-full
+ bg-zinc-900 rounded-lg scale-0 group-hover:lg:scale-125 
+  group-hover:md:scale-150 group-hover:translate-y-[5vw] group-hover:opacity-100'>
+    <Image src={movie.thumbnailUrl} alt='movie' width={200} height={200} 
+    className='cursor-pointer object-cover transition-all duration-300  shadow-xl w-full rounded-t-lg'/>
+    <div className='p-2 shadow-lg'>
+      <p>Action buttons</p>
+
+    </div>
+
+</div>
     </CardContent>
 </Card>
         </CarouselItem>
