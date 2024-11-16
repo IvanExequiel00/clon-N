0dc0036368cca0a5714b50761f7a4da6ddd89a33
IvanRojas FT47b <exequieliv@outlook.es>
2024-11-16 16:40:32 -0300

    funcionalidad fav

--- clone-netflix/app/(routes)/(home)/components/ListMovies/ListMovies.tsx
+++ clone-netflix/app/(routes)/(home)/components/ListMovies/ListMovies.tsx
@@ -1,12 +1,22 @@
+"use client"
 import { BlockMovies } from "@/components/shared/BlockMovies/BlockMovies";
 import { ListMoviesProps } from "./ListMovies.types";
+import { useLovedFilms } from "@/hooks/use-loved-films";
+import { useCurrentNetflixUser } from "@/hooks/use-current-user";
+
 
 
 export  function ListMovies(props: ListMoviesProps) {
     const { movies } = props;
+    const {lovedFilmsByUser} = useLovedFilms()
+    const {currentUser} = useCurrentNetflixUser();
+    const userNetflix = currentUser?.id;
+
+    const lovedFilm = userNetflix ? lovedFilmsByUser[userNetflix]: [];
   return (
     <div>
         <BlockMovies title="Peliculas mas recientes" movies={movies} isMyList={false}/>
+        <BlockMovies  title="Peliculas Fav" movies={lovedFilm} isMyList={true}/>
     </div>
   )
 }
